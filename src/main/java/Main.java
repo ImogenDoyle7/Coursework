d1826f48b619ce6868c3fe37769df28ec328375f
imogendoyle <87953@farnborough.ac.uk>
2019-07-09 09:57:35 +0100

    testing 09/07/2019 mac

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class Main {
+    // testing commit
     public static void main(String args[]) {
         // code to open database
         Scanner input = new Scanner(System.in);
