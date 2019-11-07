e375c85d7fc8260e4054217b9179dc362b5375b3
ImogenDoyle7 <87953@farnborough.ac.uk>
2019-11-07 14:26:35 +0000

    phase 2                                                      JSON and API methods for users controller/ list users

--- src/main/java/Controllers/UsersController.java
+++ src/main/java/Controllers/UsersController.java
@@ -1,29 +1,63 @@
 package Controllers;
 import Server.Main;
+import com.sun.jersey.multipart.FormDataParam;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.validation.constraints.Email;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-@Path("list/")
+import java.util.UUID;
+
+@Path("/Users/")
 public class UsersController {
-    @GET
-    @Path("listUsers/")
+    @POST
+    @Path("login")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
-    public String listUsers() {
-        System.out.println("listUsers/");
+    public String loginUser(@FormDataParam("Email") String Email, @FormDataParam("Password") String Password) {
+        try {
+            PreparedStatement ps1 = Main.db.prepareStatement("SELECT Password FROM Users WHERE Email = ?");
+            ps1.setString(1, Email);
+            ResultSet loginResults = ps1.executeQuery();
+            if (loginResults.next()) {
+                String correctPassword = loginResults.getString(1);
+                if (Password.equals(correctPassword)) {
+                    String token = UUID.randomUUID().toString();
+
+                    PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE Username = ?");
+                    ps2.setString(1, token);
+                    ps2.setString(2, Email);
+                    ps2.executeUpdate();
+
+                    return "{\"token\": \""+ token + "\"}";
+                } else {
+
+                    return "{\"error\": \"Unknown user!\"}";
+                }
+                } else {
+                return "{\"error\": \"Unknown user!\"}";
+            }
+        } catch (Exception exception) {
+            System.out.println("Database error during /user/login: " + exception.getMessage());
+            return "{\"error\": \"Unable to log in user, server side error!\"}";
+        }
+    }
+    @GET
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String userDetails() {
+        System.out.println("user/details");
         JSONArray list = new JSONArray();
         try {
             PreparedStatement ps = Main.db.prepareStatement("SELECT Email, Password FROM Users");
             ResultSet results = ps.executeQuery();
             while (results.next()) {
                 JSONObject item = new JSONObject();
-                item.put("Email", results.getInt(1));
-                item.put("Password", results.getString(2));
+                item.put("Email: ", results.getString(1));
+                item.put("Password: ", results.getString(2));
                 list.add(item);
             }
             return list.toString();
@@ -32,6 +66,37 @@ public class UsersController {
             return "{\"error\": \"Unable to list users, please see server console for more info.\"}";
         }
     }
+    @POST
+    @Path("new")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String newUser(@FormDataParam("Email") String Email, @FormDataParam("Password") String Password) {
+        try {
+            if (Email == null || Password == null) {
+                throw new Exception("One or more form data parameters are missing in the HTTP request.");
+            }
+                System.out.println("user/new email=" + Email);
+
+                PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users (Email, Password) VALUES (?, ?)");
+                ps.setString(1, Email);
+                ps.setString(2, Password);
+                ps.execute();
+                return "{\"status\": \"OK\"}";
+        } catch (Exception exception){
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"error\": \"Unable to create new user, please see server console for more info\"}";
+
+        }
+    }
+   /* @POST
+    @Path("editEmail")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String updateEmail(@FormDataParam("Email") String Email, @FormDataParam("Password") String Password) {
+        try {
+            loginUser(Email, Password);
+            return Email;
+    }*/
 
 
 
@@ -60,7 +125,7 @@ public class UsersController {
 
 
 
-        public static void newUsers(String Email, String Password)
+       /* public static void newUsers(String Email, String Password)
         //code to add new users data to the users table
         {
 
@@ -111,5 +176,5 @@ public class UsersController {
             System.out.println("Database error: " + exception.getMessage());
             System.out.println("Data not deleted from database");
         }
-    }
+    }*/
         }
