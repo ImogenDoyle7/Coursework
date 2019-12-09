80de996fb82d8b6f3a3432c95b4bca068b3f992d
ImogenDoyle7 <87953@farnborough.ac.uk>
2019-12-09 10:28:41 +0000

    phase 3 restructuring

--- resources/Client/js/index.js
+++ resources/Client/js/index.js
@@ -4,8 +4,8 @@ function pageLoad() {
 
     let myHTML = '<div style="text-align:center;">'
         + '<h1>Student Revision Tool</h1>'
-        + '<button type="button">Log In</button>'
-        + '<button type="button">Sign Up</button>'
+        + '<button type="LogInButton">Log In</button>'
+        + '<button type="signUpButton">Sign Up</button>'
         + '<img src="/client/img/logo.png"  alt="Logo"/>'
         + '<div style="font-style: italic;">'
         + 'Generated at ' + now.toLocaleTimeString()
@@ -18,8 +18,8 @@ function checkLogin() {
 
     let myHTML = '<div style="text-align:center;">'
         + '<h1>Student Revision Tool</h1>'
-        + '<button type="button">Log In</button>'
-        + '<button type="button">Sign Up</button>'
+        + '<button type="LogInButton">Log In</button>'
+        + '<button type="signUpButton">Sign Up</button>'
         + '<img src="/client/img/logo.png"  alt="Logo"/>'
         + '</div>'
         + '</div>';
@@ -28,36 +28,36 @@ function checkLogin() {
 }
 function checkLogin() {
 
-    let username = Cookies.get("username");
+    let email = Cookies.get("email");
 
     let logInHTML = '';
 
-    if (username === undefined) {
+    if (email === undefined) {
 
-        let editButtons = document.getElementsByClassName("editButton");
-        for (let button of editButtons) {
+        let LogInButton = document.getElementsByClassName("Log In");
+        for (let button of LogInButton) {
             button.style.visibility = "hidden";
         }
 
-        let deleteButtons = document.getElementsByClassName("deleteButton");
-        for (let button of deleteButtons) {
+        let signUpButton = document.getElementsByClassName("Sign Up");
+        for (let button of signUpButton) {
             button.style.visibility = "hidden";
         }
 
         logInHTML = "Not logged in. <a href='/client/login.html'>Log in</a>";
     } else {
 
-        let editButtons = document.getElementsByClassName("editButton");
-        for (let button of editButtons) {
+        let LogInButton = document.getElementsByClassName("Log In");
+        for (let button of LogInButton) {
             button.style.visibility = "visible";
         }
 
-        let deleteButtons = document.getElementsByClassName("deleteButton");
-        for (let button of deleteButtons) {
+        let signUpButton = document.getElementsByClassName("Sign Up");
+        for (let button of signUpButton) {
             button.style.visibility = "visible";
         }
         checkLogin();
-        logInHTML = "Logged in as " + username + ". <a href='/client/login.html?logout'>Log out</a>";
+        logInHTML = "Logged in as " + email + ". <a href='/client/login.html?logout'>Log out</a>";
 
     }
 
